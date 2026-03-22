ad96398cf93ebf1bb269349987c832c0a78fcc03
Wren Weburg <wren@weburg.com>
2026-03-21 23:36:08 -0700

    Typo fix.

--- php/views/home.php
+++ php/views/home.php
@@ -6,7 +6,7 @@ $pageTitle = "Home";
 
 <h2>GHoWSt DUETS</h2>
 
-<img src="/images/ghowstduetslogo.png" alt="GHoWSt DUET logo" width="744" height="682" style="width: auto; height: 72px; float: left; margin-right: 10px; margin-bottom: 10px;">
+<img src="/images/ghowstduetslogo.png" alt="GHoWSt DUETS logo" width="744" height="682" style="width: auto; height: 72px; float: left; margin-right: 10px; margin-bottom: 10px;">
 
 <h3>Depend Upon Existing Technology Stack</h3>
 
